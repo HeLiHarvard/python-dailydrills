9a5121fe9591fcf5d97fa7c0f61ea12cb42398aa
He Li <heli@college.harvard.edu>
2013-12-22 23:10:06 -0600

    fixed type errors - treegen

--- ez/tree-generation.py
+++ ez/tree-generation.py
@@ -6,8 +6,8 @@ tree_info = (input('Tree info: ')).split()
 
 tree_base = int(tree_info[0])
 
-if not (tree_base % 2):
-    sys.exit("Please enter an odd number")
+if not (tree_base % 2) or (tree_base < 3 or tree_base > 21) :
+    sys.exit("Please enter an odd number between 3 and 21")
 
 trunk_char = tree_info[1]
 leaf_char = tree_info[2]
@@ -15,10 +15,10 @@ leaf_char = tree_info[2]
 i = 1 #variable to count for how many tree layers to draw in while loop
 
 while i <= tree_base:
-    spaces = (tree_base - i) // 2
+    spaces = int((tree_base - i) / 2)
     print((' ' * spaces) + (leaf_char * i) + (' ' * spaces))
     i += 2
 
-spaces = (tree_base - 3) / 2
+spaces = int((tree_base - 3) / 2)
 
 print ((' ' * spaces) + (trunk_char * 3) + (' ' * spaces))
