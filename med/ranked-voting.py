0d7c881c1de45d91b25abf09cf18f51d0c898911
He Li <heli@college.harvard.edu>
2013-12-24 22:23:21 -0600

    revised ranked-voting, still bugged w inf loop

--- med/ranked-voting.py
+++ med/ranked-voting.py
@@ -1,42 +1,59 @@
 #! /usr/bin/env python3.3
 
 import sys
+from collections import OrderedDict
 
+#inputs
 [n, m] = (input('Type N and M: ')).split()
-
 candidates = (input('Type ' + m + ' candidates: ')).split()
-
 n, m = int(n), int(m)
 
+#checking exactly m candidates are entered
 if len(candidates) !=  m:
     sys.exit('Please enter exactly ' + m + ' candidates')
 
-ballots = [[] for x in range(n)]
+#initializing the vote total tracker
+vote_tracker = OrderedDict()
+for i in range(m):
+    vote_tracker[candidates[i]] = 0
 
+#ballot inputs
+ballots = [[] for x in range(n)]
 for i in range(n):
     ballots[i] = (input('Enter ballot: ')).split()
 
-vote_totals = []
 winner = ''
+loser = ''
+least_votes = n
 
-for i in range(m):
+#ENTIRE VOTE TOTALING ALGORITHM NEEDS TO BE CHANGED
+
+#for i in range(m):
+
+while winner == '':
+    print("Round " + str(i) + ": ", end = '')
     for j in range(n):
-        vote_totals[ballots[j][i]] += 1
-    for k in range(m):
-        if vote_totals[k] > n / 2:
-            winner = candidates[k]
+        for i in range(m):
+            if candidates[int(ballots[j][i])] in vote_tracker:
+                vote_tracker[candidates[int(ballots[j][i])]] += 1
+                break
+
+    vote_tracker = OrderedDict(sorted(vote_tracker.items(),
+                                     key = lambda t: t[1]))
+    for c in vote_tracker.items():
+        print(str(100 * c[1] / n) + '% ' + c[0], end = ', ')
+        if c[1] > n / 2:
+            winner = c[0]
+            print(winner + ' is the winner')
             break
+        if c[1] < least_votes:
+            least_votes = c[1]
+            loser = c[0]
     if winner != '':
         break
-
-
-print(winner)
-
-
-
-
-
-
+    del(vote_tracker[loser])
+    for k in vote_tracker.items():
+        vote_tracker[k] = 0
 
 
 
